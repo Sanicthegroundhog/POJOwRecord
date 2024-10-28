dac25496e3ad4aff9b35893b34f5aaafeb0f6b1f
Jacob Atterson <atterjac000@warren.k12.in.us>
2024-10-28 13:27:00 -0400

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,6 @@
+public record Student(String studentId,
+                      String firstName,
+                      String lastName,
+                      int age,
+                      String major) {
+}
